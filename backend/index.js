a2db3efb9fd56a45cfae0a294e6df1d872038587
Thanmay Nath <thanmaynath357@gmail.com>
2023-06-18 21:00:18 +0530

    Created Events APIs

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const user = require("./routes/userAuthentication");
+const events = require("./routes/events");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use("/user", user);
+app.use("/events", events);
 app.listen(8800, () => {
   console.log("Server started on port 8800");
 });
---
+++ backend/routes/events.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../db/config.js");
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    //console.log(file);
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.get("/", async (req, res) => {
+  try {
+    const query = `SELECT * FROM public.events`;
+    const data = await db.query(query);
+    const result = data.rows;
+    return res.send(result).status(200);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+router.post("/add", upload.single("file"), async (req, res) => {
+  try {
+    const values = [
+      req.body.userId,
+      req.body.eventName,
+      req.body.location,
+      req.body.date,
+      req.body.time,
+      req.body.price,
+      req.body.seats,
+      req.file.path,
+    ];
+    const query = `INSERT INTO public.events (user_id,name,location,date,time,price,seats,imageUrl)VALUES ($1,$2,$3,$4,$5,$6,$7,$8)`;
+    await db.query(query, values);
+    res.status(200).json({ message: "Succesfully added new event." });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error });
+  }
+});
+
+module.exports = router;
